01b100c5a1c916753129c60667c8500f8f3073c8
Giacomo Tüfekci <kontakt@tuefekci.de>
2021-11-03 21:36:41 +0100

    Update system.php

--- examples/system.php
+++ examples/system.php
@@ -9,3 +9,13 @@
 require_once(realpath(__DIR__ . '/../vendor/autoload.php'));
 
 var_dump(\tuefekci\helpers\System::isDocker());
+
+
+$storage = \tuefekci\helpers\System::getStorage();
+
+var_dump(\tuefekci\helpers\System::countCpuCores());
+var_dump(\tuefekci\helpers\System::getCpuUsage());
+var_dump(\tuefekci\helpers\System::getMemory());
+var_dump(\tuefekci\helpers\System::getMemoryUsage());
+var_dump(\tuefekci\helpers\System::getMemoryFree());
+var_dump(\tuefekci\helpers\System::getMemoryAvailable());
